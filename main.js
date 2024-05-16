98cc008b9b6e797a888c36910b1d1ce698c8cf97
Hiroregyqy3b <126179693+Hiroregyqy3b@users.noreply.github.com>
2024-05-16 12:17:10 -0300

    fgh

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-20T00:00:00");
-const tempoObjetivo2 = new Date("2024-12-12T00:00:00");
+const tempoObjetivo2 = new Date("2024-12-12T07:00:00");
 const tempoObjetivo3 = new Date("2024-12-9T00:00:00");
 const tempoObjetivo4 = new Date("2024-07-71T00:00:00");
 
