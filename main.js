bd9b95a9c1614f335f99cb9abc9a6852e8dfe463
Hiroregyqy3b <126179693+Hiroregyqy3b@users.noreply.github.com>
2024-05-24 08:14:04 -0300

    rtdtrsr

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-09-20T00:00:00");
+const tempoObjetivo1 = new Date("2024-05-27T00:00:00");
 const tempoObjetivo2 = new Date("2029-12-12T00:00:00");
 const tempoObjetivo3 = new Date("2027-12-09T00:00:00");
 const tempoObjetivo4 = new Date("2025-07-07T00:00:00");
